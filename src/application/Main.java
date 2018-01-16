6c51af173a8ab4c7109936ef6350bc8bacf3f33b
g00296814 <g00296814@gmit.ie>
2018-01-16 19:50:49 +0000

    Updates

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+        @Override
+        public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setTitle("Android Sensor Emulator");
+           // stage.setMaximized(true);
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+}
